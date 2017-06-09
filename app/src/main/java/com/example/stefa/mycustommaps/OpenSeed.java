1e5da74c5b8f1673aae1d1a8d0feae659ec42492
isabellalala <isabellaps90@hotmail.com>
2017-06-09 23:49:47 +0200

    Open seed ttile + des

--- app/src/main/java/com/example/stefa/mycustommaps/OpenSeed.java
+++ app/src/main/java/com/example/stefa/mycustommaps/OpenSeed.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,24 +15,30 @@ import com.google.firebase.database.ValueEventListener;
 
 public class OpenSeed extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_seed);
 
         DatabaseReference firebaseDB = FirebaseDatabase.getInstance().getReference();
-        String seedTitle = getIntent().getStringExtra("seed");
+        final String seedTitle = getIntent().getStringExtra("seed");
         Log.e("OpenSeed", seedTitle);
         final OpenSeed currentView = this;
 
+
         Log.e("OpenSeed", firebaseDB.child(seedTitle).toString());
+        final TextView textTitle = (TextView)findViewById(R.id.openSeedTitle);
+        final TextView textDescription = (TextView)findViewById(R.id.openSeedDes);
 
         firebaseDB.child(seedTitle).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Seed seed = dataSnapshot.getValue(Seed.class);
 
-                Toast.makeText(currentView, seed.toString(), Toast.LENGTH_SHORT).show();
+                textTitle.setText(seed.title);
+                textDescription.setText(seed.description);
             }
 
             @Override
